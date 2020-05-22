05cb3dcc021cb184b1396a5ca55d5e54e61357bf
Karol Zeluk <karol.zeluk@collibra.com>
2020-05-22 15:11:28 +0200

    netlify form

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -47,7 +47,7 @@ export default () => {
     		</div>
 		    <div style={{ width: "50%", float: "left" }} className="lefttBoxForm">
 		      <h2>Formularz kontaktowy</h2>
-		        <form action="kontakt/" method="post">
+		        <form action="kontakt/" method="POST" data-netlify="true" netlify-honeypot="bot-field">
               <fieldset>
                 Email:<br /><input type="email" name="email" placeholder="podaj adres email..." className="input_bg" maxlength="50" required="" /><br /><br />
                 Treść:<br /><textarea type="text" name="tresc" placeholder="twoje zapytanie..." style={{ height: "250px" }} className="textarea_bg" required=""></textarea>
